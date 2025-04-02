b1b84407fe98bfdf5ca04b51ded360984d900a54
stevenGrz13 <razafimahefasteven130102@gmail.com>
2025-04-02 11:47:42 +0300

    integration planning dashboard

--- src/app/features/mechanic/services/rendezvous.service.ts
+++ src/app/features/mechanic/services/rendezvous.service.ts
@@ -3,6 +3,8 @@ import { ResourceService } from '../../../core/services/resource.service';
 import { Mechanic } from '../models/mechanic.model';
 import { HttpClient } from '@angular/common/http';
 import { User } from '../../../core/models/user.model';
+import { map, Observable } from 'rxjs';
+import { Planning } from '../../appointment/models/planning.model';
 
 @Injectable({
   providedIn: 'root',
@@ -19,5 +21,13 @@ export class RendezvousService extends ResourceService<User> {
       url
     );
     return res;
+  }
+
+  getPlanning(): Observable<Planning[]> {
+    const url = `${this.apiUrl}/getPlanning`;
+    return this.http.get<{ success: boolean; data: Planning[]; message: string }>(url)
+      .pipe(
+        map(response => response.data) // On s'assure que response.data est bien un tableau
+      );
   }
 }
