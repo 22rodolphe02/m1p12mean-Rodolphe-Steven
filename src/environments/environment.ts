97e0ef49e900dc91f0db7cb96ab282e6befb7fa5
stevenGrz13 <razafimahefasteven130102@gmail.com>
2025-04-03 21:39:42 +0300

    url api prod

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://localhost:3000/garage/api',
+  // apiUrl: 'http://localhost:3000/garage/api',
+  apiUrl: 'https://m1p12mean-steven-rodolphe-backend.onrender.com/garage/api',
 };
